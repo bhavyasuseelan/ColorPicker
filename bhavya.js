85af8e157a9f6dff6e4cbc648a041c6ccb5abaf2
bhavyasuseelan <89065283+bhavyasuseelan@users.noreply.github.com>
2021-08-18 16:05:11 +0530

    Add files via upload

---
+++ bhavya.js
@@ -0,0 +1,15 @@
+function makePink(){
+    document.body.style.backgroundColor="pink";
+}
+
+function makeGreen(){
+    document.body.style.backgroundColor="green";  
+}
+
+function makeYellow(){
+    document.body.style.backgroundColor="yellow";  
+}
+
+function makeBlue(){
+    document.body.style.backgroundColor="blue";  
+}
